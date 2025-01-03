c68f113e03d713192abf4d769869b0c510353129
Leo en laptop <leonar2zb@gmail.com>
2025-01-03 19:48:54 -0300

    Implementado eliminar paciente

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,10 +1,14 @@
+import { usePatientStore } from "../store"
 import { Patient } from "../types"
 import PatientDetailsItem from "./PatientDetailsItem"
 
 type PatientDetailProps = {
     patient: Patient
 }
+
 export default function PatientDetails({ patient }: PatientDetailProps) {
+    const { deletePatient } = usePatientStore()
+
     return (
         <div className="mx-5 my-10 px-5 py-5 bg-white shadow-md rounded-xl">
             <PatientDetailsItem label='ID' data={patient.id} />
@@ -21,7 +25,8 @@ export default function PatientDetails({ patient }: PatientDetailProps) {
                 </button>
                 <button
                     type="button"
-                    className="py-2 px-10 bg-red-600 hover:bg-red-700 uppercase rounded-lg text-white font-bold">
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 uppercase rounded-lg text-white font-bold"
+                    onClick={() => deletePatient(patient.id)}>
                     Emininar
                 </button>
             </div>
