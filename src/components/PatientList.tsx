518f833a0c765cca14de7a52d2bc8518e4de7b0a
Leo en laptop <leonar2zb@gmail.com>
2025-01-03 17:37:17 -0300

    Escribiendo en el estado de zustand

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,4 +1,8 @@
+import { usePatientStore } from "../store"
+
 export default function PatientList() {
+    const patients = usePatientStore(state => state.patients)
+    console.log(patients)
     return (<>
         Patient List
     </>)
